c0992a3352e0d29f53f5d2f572114821b9855800
Kit La Touche <kit@transneptune.net>
2016-06-20 18:03:54 -0600

    Update to use libtmux.

--- tmux-phi.py
+++ tmux-phi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import tmuxp
+from libtmux import server
 
 
 def phi(n):
@@ -9,15 +9,15 @@ def phi(n):
     return (a, b)
 
 
-server = tmuxp.Server()
+server = server.Server()
 session = server.list_sessions()[0]
-window = session.attached_window()
+window = session.attached_window
 
 window_height = int(window.get('window_height'))
 window_width = int(window.get('window_width'))
 
 main_width, secondary_width = phi(window_width)
 
-current_pane = window.attached_pane()
+current_pane = window.attached_pane
 current_pane.set_width(main_width)
 current_pane.set_height(window_height)
